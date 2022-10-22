aaf90545e7ff292c05b8a6d37608d5dc91efaf06
Gabriele Bublitz <Gabrielebublitz21@hotmail.com>
2022-10-22 15:45:14 -0300

    Carousel
    Ajustes feitos, falta implementar mais coisas

--- Visual Studio/MeuSiteEmMVC/MeuSiteEmMVC/wwwroot/js/site.js
+++ Visual Studio/MeuSiteEmMVC/MeuSiteEmMVC/wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-const controls = document.querySelectorAll(".control");
+﻿const controls = document.querySelectorAll(".control");
 let currentItem = 0;
 const items = document.querySelectorAll(".item");
 const maxItems = items.length;
