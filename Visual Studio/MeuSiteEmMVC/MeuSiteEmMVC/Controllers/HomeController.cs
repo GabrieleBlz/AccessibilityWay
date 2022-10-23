060c64bc63f4361280476496f3506d77b3808ef8
NicolleF <nicolleformigari@gmail.com>
2022-10-23 20:55:31 -0300

    enfeitando forms

--- Visual Studio/MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
+++ Visual Studio/MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace MeuSiteEmMVC.Controllers
             return View();
         }
 
+        public IActionResult Simulacao()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
